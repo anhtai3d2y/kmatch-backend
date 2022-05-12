203f908093a31bee2149af3b6ef5aa29db44a857
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-05-12 10:52:11 +0700

    #7 Add swagger UI document

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get()
+  redirect(@Res() res): Promise<any> {
+    return res.redirect('/api');
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Kmatch backend')
+    .setDescription('The kmatch API ')
+    .setVersion('1.0')
+    .addTag('hello world')
+    .addTag('auth')
+    .addTag('adminUser')
+    .addTag('role')
+    .addTag('permission')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT;
   await app.listen(port);
   console.log(`Application listening on port ${port}`);
