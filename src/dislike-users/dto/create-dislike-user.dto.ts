b428440590ef684a36d6218f15c902446e5ef308
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-06-03 15:46:33 +0700

    Add authentication for create and get return more info about user disliked

--- src/dislike-users/dto/create-dislike-user.dto.ts
+++ src/dislike-users/dto/create-dislike-user.dto.ts
@@ -7,16 +7,6 @@ export class CreateDislikeUserDto {
   @IsMongoId()
   @ApiProperty({
     type: String,
-    description: 'userId',
-    example: '627e17990d52eb29d86831cd',
-  })
-  userId: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @IsMongoId()
-  @ApiProperty({
-    type: String,
     description: 'userDislikedId',
     example: '62846ff7dc8f605051b07c73',
   })
