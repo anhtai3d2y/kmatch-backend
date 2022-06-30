8d32ece6b71674303d43dd1c10e014639abbdcb2
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-06-30 13:52:56 +0700

    Complete payment with paypal for various type of package like role, boots and star

--- src/paypal/paypal.module.ts
+++ src/paypal/paypal.module.ts
@@ -3,14 +3,18 @@ import { PaypalService } from './paypal.service';
 import { PaypalController } from './paypal.controller';
 import { MessageErrorModule } from 'src/message-error/message-error.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { paypalSchema } from './schemas/messages.schema';
+import { paypalSchema } from './schemas/paypal.schema';
 import { userSchema } from 'src/users/schemas/user.schema';
+import { superlikeStarSchema } from 'src/superlike-star/schemas/superlike-star.schema.';
+import { bootsSchema } from 'src/boots/schemas/boots.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Paypal', schema: paypalSchema },
       { name: 'User', schema: userSchema },
+      { name: 'SuperlikeStar', schema: superlikeStarSchema },
+      { name: 'Boots', schema: bootsSchema },
     ]),
     MessageErrorModule,
   ],
