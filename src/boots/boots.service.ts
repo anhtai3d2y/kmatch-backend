2aa428b17f144c32221d704c5a5b1529226592fa
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-06-30 14:37:01 +0700

    Create an endpoint for user use boots

--- src/boots/boots.service.ts
+++ src/boots/boots.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { User } from 'src/users/interfaces/user.interfaces';
 import { CreateBootDto } from './dto/create-boot.dto';
 import { Boots } from './interfaces/boots.interfaces';
 
@@ -9,6 +10,8 @@ export class BootsService {
   constructor(
     @InjectModel('Boots')
     private readonly bootsModel: Model<Boots>,
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
   ) {}
 
   async create(createBootDto: CreateBootDto, user) {
@@ -37,5 +40,39 @@ export class BootsService {
       userId: user._id,
     });
     return data;
+  }
+
+  async useBoots(user) {
+    const boots = await this.bootsModel.findOne({
+      userId: user._id,
+    });
+    if (boots && boots.amount > 0) {
+      const userBoots = await this.userModel.findOne({
+        _id: user._id,
+      });
+      // userBoots.boots = Date.now();
+      const bootsPlus = 30 * 60 * 1000;
+      let bootsTime;
+      if (userBoots && userBoots.boots && userBoots.boots < Date.now()) {
+        bootsTime = Date.now();
+      } else {
+        bootsTime = userBoots.boots + bootsPlus;
+      }
+      await userBoots.updateOne({
+        boots: bootsTime + bootsPlus,
+      });
+      await boots.updateOne({
+        amount: boots.amount - 1,
+      });
+    } else {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+          message: "You don't have enough boots. Please buy more boots!",
+          error: 'Unprocessable Entity',
+        },
+        400,
+      );
+    }
   }
 }
