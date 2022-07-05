d633f6e58c635f6964d0e9c3f5d185c9203b932d
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-07-05 16:51:16 +0700

    Create a basic check when use superlike star

--- src/superlike-users/superlike-users.service.ts
+++ src/superlike-users/superlike-users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { SuperlikeStar } from 'src/superlike-star/interfaces/superlike-star.interfaces';
 import { CreateSuperlikeUserDto } from './dto/create-superlike-user.dto';
 import { SuperlikeUsers } from './interfaces/superlike-users.interfaces';
 
@@ -9,14 +10,34 @@ export class SuperlikeUsersService {
   constructor(
     @InjectModel('SuperlikeUsers')
     private readonly superlikeUserModel: Model<SuperlikeUsers>,
+    @InjectModel('SuperlikeStar')
+    private readonly superlikeStarModel: Model<SuperlikeStar>,
   ) {}
   async create(createSuperlikeUserDto: CreateSuperlikeUserDto, user) {
     const userId = user._id.toString();
-    const superlike = await this.superlikeUserModel.create({
+    const superlikeStar = await this.superlikeStarModel.findOne({
       userId: userId,
-      userSuperlikedId: createSuperlikeUserDto.userSuperlikedId,
     });
-    return superlike;
+    console.log(superlikeStar);
+    if (superlikeStar && superlikeStar.amount > 0) {
+      const superlike = await this.superlikeUserModel.create({
+        userId: userId,
+        userSuperlikedId: createSuperlikeUserDto.userSuperlikedId,
+      });
+      await superlikeStar.updateOne({
+        amount: superlikeStar.amount - 1,
+      });
+      return superlike;
+    } else {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+          message: "You don't have enought super like star!",
+          error: 'Unprocessable Entity',
+        },
+        400,
+      );
+    }
   }
 
   async findAll(user) {
