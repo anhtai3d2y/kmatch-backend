e870c015809e4e7fcbbd4874b1da94dff0faa281
Pham Duy Tai <tai.opentechiz@gmail.com>
2022-07-12 15:26:14 +0700

    #39 Return result is matched or not when like or superlike user

--- src/like-users/like-users.module.ts
+++ src/like-users/like-users.module.ts
@@ -4,9 +4,13 @@ import { LikeUsersController } from './like-users.controller';
 import { likeUsersSchema } from './schemas/like-users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessageErrorModule } from 'src/message-error/message-error.module';
+import { superlikeUsersSchema } from 'src/superlike-users/schemas/superlike-users.schema';
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'LikeUsers', schema: likeUsersSchema }]),
+    MongooseModule.forFeature([
+      { name: 'LikeUsers', schema: likeUsersSchema },
+      { name: 'SuperlikeUsers', schema: superlikeUsersSchema },
+    ]),
     MessageErrorModule,
   ],
   controllers: [LikeUsersController],
